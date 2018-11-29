0206e6d0487010de2924037b12b04ff5f0481ee7
Kostya <koortttt@gmail.com>
2018-11-29 16:34:05 +0200

    ne gruzit nihuya boje blyat' ya ustal

--- js/google-maps-api.js
+++
@@ -1,8 +0,0 @@
-// function initMap() {
-//     var kyiv = {lat: 50.4501, lng: 30.523400000000038};
-//     // The map, centered at Kyiv
-//     var map = new google.maps.Map(
-//         document.getElementById('map'), {zoom: 15, center: kyiv});
-//     // The marker, positioned at Kyiv
-//     var marker = new google.maps.Marker({position: kyiv, map: map});
-// }
